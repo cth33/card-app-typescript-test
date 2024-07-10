fedd55afff91e6f5a666b3ee04857e32071e1443
cth33 <ctheocharousj@gmail.com>
2024-07-10 16:37:41 +0100

    Implementation of simple changes in frontend functionality to properly display dates in the cards.

--- frontend/src/routes/AllEntries.tsx
+++ frontend/src/routes/AllEntries.tsx
@@ -21,13 +21,21 @@ export default function AllEntries(){
                     <div id={entry.id} key={index}className="bg-gray-300 shadow-md shadow-gray-500 m-3 p-4 rounded flex flex-col justify-between">
                         <h1 className="font-bold text-sm md:text-lg">{entry.title}</h1>
                         <p className="text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3">{entry.description}</p>
-                        <section className="flex items-center justify-between flex-col md:flex-row pt-2 md:pt-0">
-                        <div className="flex justify-center">
-                            <button onClick={()=> {deleteEntry(entry.id as string)}} className="m-1 md:m-2 p-1 font-semibold rounded-md bg-red-500 hover:bg-red-700">✖</button>
-                            <button onClick={()=> {navigate(`/edit/${entry.id}`, { replace: true });}} className="m-1 md:m-2 p-1 font-semibold rounded-md bg-blue-500 hover:bg-blue-700">🖊</button>
-                        </div>
-                        <time className="text-right text-sm md:text-lg">{new Date(entry.created_at.toString()).toLocaleDateString()}</time>
-                        <time className="text-right text-sm md:text-lg">{new Date(entry.scheduled_at.toString()).toLocaleDateString()}</time>
+                        <section className="flex items-center justify-between pt-2 md:pt-0">
+                            <div className="flex justify-center">
+                                <button onClick={()=> {deleteEntry(entry.id as string)}} className="m-1 md:m-2 p-1 font-semibold rounded-md bg-red-500 hover:bg-red-700">✖</button>
+                                <button onClick={()=> {navigate(`/edit/${entry.id}`, { replace: true });}} className="m-1 md:m-2 p-1 font-semibold rounded-md bg-blue-500 hover:bg-blue-700">🖊</button>
+                            </div>
+                            <div className="flex flex-col items-end text-sm md:text-base">
+                                <div className="flex flex-col items-end">
+                                    <span className="font-semibold">Created At</span>
+                                    <time>{new Date(entry.created_at.toString()).toLocaleDateString()}</time>
+                                </div>
+                                <div className="flex flex-col items-end mt-1">
+                                    <span className="font-semibold">Scheduled At</span>
+                                    <time>{new Date(entry.scheduled_at.toString()).toLocaleDateString()}</time>
+                                </div>
+                            </div>
                         </section>
                         
                     </div>
